9a4f6ce76eddf56dbcfdf92c8b38f029a96b8970
Andy <aduly@uwo.ca>
2019-02-02 14:14:44 -0500

    Commit 001 (02/02/2019): First commit; basic/initial front-end set up.

---
+++ script.js
@@ -0,0 +1,14 @@
+// Player variables
+var player1Hold, player2Hold, player1Turn, player2Turn;
+
+player1Hold = 0;
+player2Hold = 0;
+
+player1Turn = 0;
+player2Turn = 0;
+
+document.getElementById('player1Hold').textContent = player1Hold;
+document.getElementById('player2Hold').textContent = player2Hold;
+
+document.getElementById('player1Turn').textContent = player1Turn;
+document.getElementById('player2Turn').textContent = player2Turn;
